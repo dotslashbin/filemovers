8ecea7e6ad557fb927fc8a4b1338d1478b9df8b1
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-08-30 16:32:12 +0800

    Added file looper

--- mover.sh
+++ mover.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Sets the max age
 MAXAGE=$(bc <<<'24*60*60') # seconds in 28 hours
+
+for logfile in $(find ./ -maxdepth 1 -type f | grep -E '(.txt|.log)$'); do
+	echo $logfile
+done
 
 # file age in seconds = current_time - file_modification_time.
 FILEAGE=$(($(date +%s) - $(stat -c '%Y' "$1")))
 
-# Tests if a file is older than the max age of 24 hours 
+# Tests if a file is older than the max age of 24 hours
 if [ $FILEAGE -gt $MAXAGE ]; then
 	echo "older"
 fi
