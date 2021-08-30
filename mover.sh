8e20be0628c2ff57e20d4b73180e84d8d799cd99
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-08-30 17:15:54 +0800

    Revised implementation to use find and added acceptance of params

--- mover.sh
+++ mover.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Sets the max age
-MAXAGE=$(bc <<<'24*60*60') # seconds in 28 hours
+# Sets the max age 5 days
+MAXAGE=5
+FOLDER=$1
+FORMAT=$2
+DESTINAITON=$2
 
-for logfile in $(find ./ -maxdepth 1 -type f | grep -E '(.txt|.log)$'); do
-	echo $logfile
+for logfile in $(find $FOLDER -maxdepth 1 -mtime $MAXAGE -type f | grep -E '(.txt|.log)$' ); do
+	ls -asl $logfile
+
+	# Moves file to destinaiton
+	# TODO: change this to mv 
+	cp -vf $logfile $DESTINAITON
 done
 
-# file age in seconds = current_time - file_modification_time.
-FILEAGE=$(($(date +%s) - $(stat -c '%Y' "$1")))
-
-# Tests if a file is older than the max age of 24 hours
-if [ $FILEAGE -gt $MAXAGE ]; then
-	echo "older"
-fi
