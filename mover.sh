14ddfd57420f8d3e35063b01c64155ee4358ee83
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-08-30 17:50:55 +0800

    Updated  with simple validation and added the maxdays back in the script

--- mover.sh
+++ mover.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-# Sets the max age 5 days
-MAXAGE=5
+# Sets the max age 5 days\
+MAXDAYS=5
+
 FOLDER=$1
 DESTINAITON=$2
 
-for logfile in $(find $FOLDER -maxdepth 1 -mtime $MAXAGE -type f | grep -E '(.txt|.log)$' ); do
+if [[ -z "$MAXDAYS" ]]; then
+	echo "You must set the max days from the makefile..."
+	exit 1
+fi
+
+if [ "$#" -ne 2 ]; then 
+	echo "You must supply the required and valid inputs."
+	echo "Sample: ./mover <source-folder> <destination-folder>"
+	exit 1
+fi
+
+for logfile in $(find $FOLDER -maxdepth 1 -mtime $MAXDAYS -type f | grep -E '(.txt|.log)$'); do
 	ls -asl $logfile
 
 	# Moves file to destinaiton
-	# TODO: change this to mv 
+	# TODO: change this to mv
 	cp -vf $logfile $DESTINAITON
 done
-
